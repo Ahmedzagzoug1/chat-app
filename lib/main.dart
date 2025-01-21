0696db0f21c2e04502e981f93a18cc1eb5a88ef3
Ahmedzagzoug1 <https://github.com/Ahmedzagzoug1>
2025-01-21 02:22:30 +0200

    first addition

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/resgister_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+// Replace with actual values
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyAWwJ7dyzNXlITmu_p8Z7dwkAynWFpxdu4',
+    appId: '1:184993023076:ios:008df4ad7c90a12ce98b71',
+    messagingSenderId: '184993023076',
+    projectId: 'chat-app-b0c25',
+  ));
+  runApp(const ScholarChat());
+}
+
+class ScholarChat extends StatelessWidget {
+  const ScholarChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        LoginPage.id: (context) => const LoginPage(),
+        RegisterPage.id: (context) => const RegisterPage(),
+        ChatPage.id: (context) => ChatPage(),
+      },
+      initialRoute: LoginPage.id,
+    );
+  }
+}
